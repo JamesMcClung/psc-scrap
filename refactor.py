8adb895fdb361e821eda32f0c77c90a3d6fbeed7
James McClung <james@themcclungs.net>
2021-07-09 14:54:08 -0400

    added other B inputs

--- refactor.py
+++ refactor.py
@@ -2,8 +2,8 @@
 
 import csv
 
-rawfile = "case1-input-phi.txt"
-newfile = "../psc/input/bgk-input-1-phi.txt"
+rawfile = "case1-medB.txt"
+newfile = "../psc/input/bgk-input-1-medB.txt"
 
 # load the data
 with open(rawfile) as input, open(newfile, "w") as output:
