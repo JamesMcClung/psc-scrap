80b1676665f9ef68ae39c03d49c776be565893eb
James <jm1667@wildcats.unh.edu>
2023-01-18 16:46:35 -0500

    autofigs: generate sequence plots

--- autofigs.py
+++ autofigs.py
@@ -1,11 +1,14 @@
 #!/mnt/lustre/IAM851/jm1667/psc-scrap/env/bin/python3
 import sys
+import os
 import yaml
 import dotenv
 
 sys.path.append(dotenv.dotenv_values()["PYTHONPATH"])
 
 import bgk
+import matplotlib.pyplot as plt
+import numpy as np
 
 ########################################################
 
@@ -49,5 +52,47 @@ for item in config["instructions"]:
 
     ##########################
 
-    for param_str in set(item["sequences"] + item["profiles"] + item["videos"]):
-        print(f"Handling {param_str}")
+    for param_str in set(item["sequences"] + item["profiles"] + item["videos"] + item["stabilities"]):
+        if param_str in item["profiles"]:
+            pass
+        if param_str in item["videos"]:
+            pass
+        if param_str in item["sequences"]:
+
+            # Move this section out once profiles/videos are implemented:
+            param = bgk.run_params.__dict__[param_str]
+            videoMaker.loadData(param)
+            videoMaker.setSlice(centerSlice)
+
+            ##########################
+
+            minima = videoMaker.getLocalExtremaIndices(np.less)
+
+            startFrame = 0
+            endFrame = minima[0]
+            titleText = "over First Oscillation"
+
+            ##########################
+
+            plt.close("all")
+            nStillFrames = 5
+
+            fig, axs = plt.subplots(1, nStillFrames)
+            stillFrames = [startFrame + round(i * (endFrame - startFrame) / (nStillFrames - 1)) for i in range(nStillFrames)]
+
+            for frame, ax in zip(stillFrames, axs):
+                videoMaker.viewFrame(frame, fig, ax, minimal=True)
+                ax.set_title(f"$t={videoMaker.times[frame]:.2f}$")
+                ax.tick_params("both", which="both", labelbottom=False, labelleft=frame == stillFrames[0])
+            fig.suptitle(f"Snapshots of {param.title} for $B_0={B}$ {titleText}")
+            fig.tight_layout(pad=0)
+            fig.set_size_inches(9, 2.5)
+
+            fig.subplots_adjust(right=0.9)
+            cbar_ax = fig.add_axes([0.91, 0.2, 0.01, 0.56])
+            fig.colorbar(axs[0].images[0], cax=cbar_ax)
+
+            ##########################
+
+            fig_name = f"sequence_{param_str}_B{B}_n{res}_v{'-' if ve_coef<0 else '+'}.png"
+            fig.savefig(os.path.join(outdir, fig_name), bbox_inches="tight", pad_inches=0.01, dpi=300)
