d18800f15eae376c516a986f8befdc106c9c7216
James <jm1667@wildcats.unh.edu>
2023-01-20 14:16:02 -0500

    bgk (particle_reader): put into class

--- bgk/particle_reader.py
+++ bgk/particle_reader.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import matplotlib.figure as mplf
 import numpy as np
 import pandas as pd
 
@@ -8,7 +9,7 @@ from .input_reader import Input
 ##########################
 
 
-def read_step(step: int, electronsOnly: bool = True) -> pd.DataFrame:
+def _read_step(path: str, step: int, electronsOnly: bool = True) -> pd.DataFrame:
     rank = 0
     df = pd.read_hdf(path + f"prt.{step:06d}_p{rank:06d}.h5", "particles/p0/1d")
     df.drop(columns=["x", "px", "m", "w", "tag"], inplace=True)
@@ -21,49 +22,50 @@ def read_step(step: int, electronsOnly: bool = True) -> pd.DataFrame:
 
 ##########################
 
-path = "/mnt/lustre/IAM851/jm1667/psc-runs/case1/single_truedist/B1/"
 
-inputFile = readParam(path, "path_to_data", str)
-B = readParam(path, "H_x", float)
-maxStep = readParam(path, "nmax", int)
-ve_coef = readParam(path, "v_e_coef", float)
+class ParticleReader:
+    def __init__(self, path: str) -> None:
+        self.path = path
 
-##########################
+        self.inputFile = readParam(path, "path_to_data", str)
+        self.B = readParam(path, "H_x", float)
+        self.maxStep = readParam(path, "nmax", int)
+        self.ve_coef = readParam(path, "v_e_coef", float)
 
-step = 0
-t: float = Loader(path, engine="pscadios2", species_names=["e", "i"])._get_xr_dataset("pfd", step).time
+    def read_step(self, step: int) -> None:
+        self.t: float = Loader(self.path, engine="pscadios2", species_names=["e", "i"])._get_xr_dataset("pfd", step).time
 
-df = read_step(step)
-input = Input(inputFile)
+        self.df = _read_step(self.path, step)
+        self.input = Input(self.inputFile)
 
-df["r"] = (df.y**2 + df.z**2) ** 0.5
-df["v_phi"] = (df.pz * df.y - df.py * df.z) / df.r
-df["v_rho"] = (df.py * df.y + df.pz * df.z) / df.r
-df.v_rho.fillna(0, inplace=True)
-df.v_phi.fillna(0, inplace=True)
-# df["w"] = (df.py ** 2 + df.pz ** 2) / 2 - df.r.map(lambda r: input.interpolate_value(r, "Psi"))
-# df["l"] = 2 * df.r * df.v_phi - B * df.r ** 2
+        df = self.df
+        df["r"] = (df.y**2 + df.z**2) ** 0.5
+        df["v_phi"] = (df.pz * df.y - df.py * df.z) / df.r
+        df["v_rho"] = (df.py * df.y + df.pz * df.z) / df.r
+        df.v_rho.fillna(0, inplace=True)
+        df.v_phi.fillna(0, inplace=True)
 
-##########################
+    def plot_distribution(self, fig: mplf.Figure = None, ax: plt.Axes = None) -> tuple[mplf.Figure, plt.Axes]:
+        if not (fig or ax):
+            fig, ax = plt.subplots()
 
-hist, rhos, v_phis = np.histogram2d(df.r, df.v_phi, bins=[60, 80])
+        hist, rhos, v_phis = np.histogram2d(self.df.r, self.df.v_phi, bins=[60, 80])
 
-v_phis_cc = (v_phis[1:] + v_phis[:-1]) / 2
-rhos_cc = (rhos[1:] + rhos[:-1]) / 2
-fs2d = hist.T / rhos_cc
+        v_phis_cc = (v_phis[1:] + v_phis[:-1]) / 2
+        rhos_cc = (rhos[1:] + rhos[:-1]) / 2
+        fs2d = hist.T / rhos_cc
 
-mean_v_phis = fs2d.T.dot(v_phis_cc) / fs2d.sum(axis=0)
-mean_v_phis_input = np.array([input.interpolate_value(rho, "v_phi") for rho in rhos_cc])
+        mean_v_phis = fs2d.T.dot(v_phis_cc) / fs2d.sum(axis=0)
+        mean_v_phis_input = np.array([self.input.interpolate_value(rho, "v_phi") for rho in rhos_cc])
 
-##########################
+        mesh = ax.pcolormesh(rhos, v_phis, fs2d, cmap="Reds")
+        ax.set_xlabel("$\\rho$")
+        ax.set_ylabel("$v_\\phi$")
+        ax.set_ylim(-0.003, 0.003)
+        ax.plot(rhos_cc, mean_v_phis, "k", label="actual mean")
+        ax.plot(rhos_cc, mean_v_phis_input, "b", label="target mean")
+        ax.set_title(f"f($\\rho$, $v_\\phi$) at t={self.t:.3f} for $B={self.B}$")
+        ax.legend()
+        fig.colorbar(mesh)
 
-plt.pcolormesh(rhos, v_phis, fs2d, cmap="Reds")
-plt.xlabel("$\\rho$")
-plt.ylabel("$v_\\phi$")
-plt.plot(rhos_cc, mean_v_phis, "k", label="actual mean")
-plt.plot(rhos_cc, mean_v_phis_input, "b", label="target mean")
-plt.title(f"f($\\rho$, $v_\\phi$) at t={t:.3f} for $B={B}$")
-plt.colorbar()
-plt.legend()
-plt.ylim(-0.003, 0.003)
-plt.show()
+        return fig, ax
