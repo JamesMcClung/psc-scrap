d3b2d7200fa1a54feb0deac556471c0473e8170b
James <jm1667@wildcats.unh.edu>
2023-04-19 13:23:38 -0400

    autofigs (sequence): fix aspect ratios*
    *uses updated matplotlib width_ratios arg to sublots

--- sequence.py
+++ sequence.py
@@ -8,7 +8,12 @@ class Sequence:
         self.step_idxs = step_idxs
         self.times = times
 
-        self.fig, self.axs = plt.subplots(n_rows, len(self.step_idxs) + 1, squeeze=False)  # +1 col for the cmap
+        self.fig, self.axs = plt.subplots(
+            n_rows,
+            len(times) + 1,  # +1 col for the cmap
+            squeeze=False,
+            width_ratios=[8] * len(times) + [1],
+        )
 
     def plot_row_pfd(self, row_idx: int, videoMaker: bgk.VideoMaker) -> None:
         axs_row = self.axs[row_idx]
@@ -19,8 +24,8 @@ class Sequence:
             _, _, im = videoMaker.viewFrame(frame, self.fig, ax, minimal=True)
             ax.set_title(f"$t={time:.2f}$" if row_idx == 0 else "")
             ax.tick_params("both", which="both", labelbottom=row_idx == len(self.axs) - 1, labelleft=step_idx == self.step_idxs[0])
-            ax.set_aspect(1)
-        cmap_ax.set_aspect(20)
+            ax.set_aspect("auto")
+        cmap_ax.set_aspect("auto")
         self.fig.colorbar(im, cax=cmap_ax)
 
     def plot_row_prt(self, row_idx: int, particles: bgk.ParticleReader, param: str) -> None:
@@ -32,11 +37,10 @@ class Sequence:
             ax.set_title(f"$t={time}$" if row_idx == 0 else "")
             ax.tick_params("both", which="both", labelbottom=row_idx == len(self.axs) - 1, labelleft=step_idx == self.step_idxs[0])
             ax.set_aspect("auto")
-        cmap_ax.set_aspect(20)
+        cmap_ax.set_aspect("auto")
         self.fig.colorbar(mesh, cax=cmap_ax)
 
     def get_fig(self, title: str) -> mplf.Figure:
-        self.fig.set_size_inches(4.5 * len(self.axs), 2.5 * len(self.axs[0]))
         self.fig.suptitle(title)
-        self.fig.tight_layout(pad=0)
+        self.fig.set_size_inches(2 * len(self.axs[0]), 2 * len(self.axs))
         return self.fig
