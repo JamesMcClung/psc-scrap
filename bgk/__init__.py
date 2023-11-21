5f49ff472a2833f282349bac80b0d6c09cc6c03b
James <jm1667@wildcats.unh.edu>
2023-11-21 13:21:54 -0500

    bgk/bounds; init: +

--- bgk/__init__.py
+++ bgk/__init__.py
@@ -8,6 +8,7 @@ from .output_reader import *
 from .input_reader import *
 from .particle_reader import *
 from .backend import *
+from .bounds import *
 from . import run_params
 
 from . import autofigs
