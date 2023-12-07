13f6780fa1cb01fdfda100195622b94e5ad521e7
James <jm1667@wildcats.unh.edu>
2023-12-07 15:29:55 -0500

    util: +binned_mean

--- bgk/autofigs/util.py
+++ bgk/autofigs/util.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 # imports used for linting
 from matplotlib.figure import Figure
@@ -11,6 +12,15 @@ from ..output_reader import VideoMaker
 def get_mean(data: DataArray, r: float, dr: float, videoMaker: VideoMaker) -> float:
     rslice = data.where((r <= videoMaker.grid_rho) & (videoMaker.grid_rho < r + dr))
     return rslice.mean().item()
+
+
+def binned_mean(data: DataArray, coord: str, nbins: int, lower: float | None = None, upper: float | None = None) -> DataArray:
+    if lower is None:
+        lower = float(data.coords[coord].min())
+    if upper is None:
+        upper = float(data.coords[coord].max())
+
+    return data.groupby_bins(coord, np.linspace(lower, upper, nbins + 1, endpoint=True), right=False).mean()
 
 
 def save_fig(fig: Figure, path: str, close: bool = False):
