4aed0b147e29427f82f0c4c0bca3bbe90341dac0
James <jm1667@wildcats.unh.edu>
2023-12-18 15:17:20 -0500

    autofigs/stability: +

--- bgk/autofigs/__init__.py
+++ bgk/autofigs/__init__.py
@@ -1,6 +1,7 @@
 from .profiles import plot_profiles, plot_extrema
 from .periodogram import *
 from .sequence import *
+from .stability import *
 from .movie import *
 from . import options
 from . import figdata
---
+++ bgk/autofigs/stability.py
@@ -0,0 +1,22 @@
+import scipy.signal as sig
+
+from . import util
+from bgk.output_reader import VideoMaker
+
+# imports used for linting
+from matplotlib.figure import Figure
+from matplotlib.pyplot import Axes
+
+
+__all__ = ["plot_stability"]
+
+
+def plot_stability(videoMaker: VideoMaker, fig: Figure = None, ax: Axes = None) -> tuple[Figure, Axes]:
+    fig, ax = util.ensure_fig_ax(fig, ax)
+
+    ax.set_xlabel("Time")
+    ax.set_ylabel("2-Norm of Difference")
+    ax.set_title(f"Deviation from ICs of {videoMaker.view_bounds.adjective}{videoMaker.param.title} ($B_0={videoMaker.params_record.B0}$, {videoMaker._case_name})")
+
+    ax.plot(videoMaker.axis_t, videoMaker._getNormsOfDiffs())
+    return fig, ax
