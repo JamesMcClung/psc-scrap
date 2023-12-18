53df78df513bdbb5b03af3bceee6684ad7b51f2a
James <jm1667@wildcats.unh.edu>
2023-12-18 11:51:58 -0500

    autofigs/movie; *: don't return AxesImage from view_frame

--- bgk/autofigs/movie.py
+++ bgk/autofigs/movie.py
@@ -6,14 +6,13 @@ from bgk.output_reader import VideoMaker
 # imports used for linting
 from matplotlib.figure import Figure
 from matplotlib.pyplot import Axes
-from matplotlib.image import AxesImage
 from matplotlib.animation import FuncAnimation
 
 __all__ = ["make_movie", "view_frame"]
 
 
 def make_movie(videoMaker: VideoMaker, fig: Figure = None, ax: Axes = None) -> tuple[Figure, FuncAnimation]:
-    fig, ax, im = view_frame(videoMaker, 0, fig, ax)
+    fig, ax = view_frame(videoMaker, 0, fig, ax)
     fig.tight_layout(pad=0)
     return fig, view_movie(videoMaker, fig, ax)
 
@@ -22,7 +21,7 @@ def _update_title(ax: Axes, videoMaker: VideoMaker, frame: int) -> None:
     ax.set_title(f"{videoMaker.view_bounds.adjective}{videoMaker.param.title}, t={videoMaker.axis_t[frame]:.3f} ($B_0={videoMaker.params_record.B0}$, {videoMaker._case_name})")
 
 
-def view_frame(videoMaker: VideoMaker, frame: int, fig: Figure = None, ax: Axes = None, minimal: bool = False) -> tuple[Figure, Axes, AxesImage]:
+def view_frame(videoMaker: VideoMaker, frame: int, fig: Figure = None, ax: Axes = None, minimal: bool = False) -> tuple[Figure, Axes]:
     fig, ax = util.ensure_fig_ax(fig, ax)
 
     im = ax.imshow(
@@ -41,7 +40,7 @@ def view_frame(videoMaker: VideoMaker, frame: int, fig: Figure = None, ax: Axes 
         plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment="right")
         fig.colorbar(im, ax=ax)
 
-    return fig, ax, im
+    return fig, ax
 
 
 def view_movie(videoMaker: VideoMaker, fig: Figure, ax: Axes) -> FuncAnimation:
