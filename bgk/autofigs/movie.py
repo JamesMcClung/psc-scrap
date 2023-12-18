9c9bcf97fb8b04a925434d6b1d918e4d278f325a
James <jm1667@wildcats.unh.edu>
2023-12-18 11:58:45 -0500

    autofigs/movie: reorder functions

--- bgk/autofigs/movie.py
+++ bgk/autofigs/movie.py
@@ -11,19 +11,6 @@ from matplotlib.animation import FuncAnimation
 __all__ = ["make_movie", "view_frame"]
 
 
-def make_movie(videoMaker: VideoMaker, fig: Figure = None, ax: Axes = None) -> tuple[Figure, FuncAnimation]:
-    fig, ax = view_frame(videoMaker, 0, fig, ax)
-    fig.tight_layout(pad=0)
-    im = ax.get_images()[0]
-
-    def update_im(frame: int):
-        im.set_array(videoMaker.datas[frame])
-        _update_title(ax, videoMaker, frame)
-        return [im]
-
-    return fig, FuncAnimation(fig, update_im, interval=30, frames=videoMaker.nframes, repeat=False, blit=True)
-
-
 def _update_title(ax: Axes, videoMaker: VideoMaker, frame: int) -> None:
     ax.set_title(f"{videoMaker.view_bounds.adjective}{videoMaker.param.title}, t={videoMaker.axis_t[frame]:.3f} ($B_0={videoMaker.params_record.B0}$, {videoMaker._case_name})")
 
@@ -48,3 +35,16 @@ def view_frame(videoMaker: VideoMaker, frame: int, fig: Figure = None, ax: Axes 
         fig.colorbar(im, ax=ax)
 
     return fig, ax
+
+
+def make_movie(videoMaker: VideoMaker, fig: Figure = None, ax: Axes = None) -> tuple[Figure, FuncAnimation]:
+    fig, ax = view_frame(videoMaker, 0, fig, ax)
+    fig.tight_layout(pad=0)
+    im = ax.get_images()[0]
+
+    def update_im(frame: int):
+        im.set_array(videoMaker.datas[frame])
+        _update_title(ax, videoMaker, frame)
+        return [im]
+
+    return fig, FuncAnimation(fig, update_im, interval=30, frames=videoMaker.nframes, repeat=False, blit=True)
