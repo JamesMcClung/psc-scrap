d64860afb6b5d3485c15ed08c4c0400043005738
James <jm1667@wildcats.unh.edu>
2024-01-16 15:04:45 -0500

    typing: +H5{Prt,Wrapper}VariableName

--- bgk/typing.py
+++ bgk/typing.py
@@ -1,10 +1,15 @@
-__all__ = ["PrefixBP", "PrefixH5", "Centering", "Dim"]
+__all__ = ["PrefixBP", "PrefixH5", "Centering", "Dim", "H5PrtVariableName", "H5WrapperVariableName"]
 
 from typing import Literal
 
 
 PrefixBP = Literal["pfd", "pfd_moments", "gauss"]
-PrefixH5 = Literal["prt"]
+# todo: BP{Pfd,PfdMoments,Gauss}VariableName, FieldVariableName
 
+PrefixH5 = Literal["prt"]
+H5PrtVariableName = Literal["x", "y", "z", "px", "py", "pz", "q", "m", "w", "tag", "id"]
+H5WrapperVariableName = H5PrtVariableName | Literal["rho", "phi", "prho", "pphi", "vx", "vy", "vz", "vrho", "vphi"]
+
+Species = Literal["e", "i"]
 Centering = Literal["cc", "nc"]
 Dim = Literal["x", "y", "z"]
