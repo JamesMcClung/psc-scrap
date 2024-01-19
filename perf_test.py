3618e0191cebc8ed6295e406fea5037ba6016b5e
James <jm1667@wildcats.unh.edu>
2024-01-19 13:38:00 -0500

    perf_test: WrapperH5 tests

--- perf_test.py
+++ perf_test.py
@@ -1,6 +1,7 @@
 import sys
 
 import bgk
+import bgk.backend.wrapper_h5 as wrapper_h5
 
 
 do_memory_tracking = len(sys.argv) > 1 and sys.argv[1] in ["-m", "--mem"]
@@ -18,22 +19,19 @@ if do_memory_tracking:
     gc.disable()
 
 
-run_manager = bgk.RunManager("/mnt/lustre/IAM851/jm1667/psc-runs/case1/trials/exact/B00.25-n128")
-vm = bgk.VideoMaker(11, run_manager)
+# run_manager = bgk.RunManager("/mnt/lustre/IAM851/jm1667/psc-runs/case1/trials/exact/B00.25-n128")
+# vm = bgk.VideoMaker(11, run_manager)
 
-vm.set_param(bgk.run_params.ne)
-vm.set_view_bounds(bgk.Bounds3D.whole())
-
-check_mem()
-
-print(vm.datas.name)
+# vm.set_param(bgk.run_params.ne)
+# vm.set_view_bounds(bgk.Bounds3D.whole())
 
 check_mem()
 
-vm.set_param(bgk.run_params.ni)
+w = wrapper_h5.load_h5("/mnt/lustre/IAM851/jm1667/psc-runs/case1/trials/exact/B00.25-n128", "prt", 0)
+print(w.col("x"))
+print(w.col("rho"))
 
 check_mem()
 
-print(vm.datas.name)
 
-check_mem()
+# print(w.head())
