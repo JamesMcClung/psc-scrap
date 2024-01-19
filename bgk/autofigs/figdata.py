3e52bb960d2225b9673f27eaf16f2fd3f5f9f032
James <jm1667@wildcats.unh.edu>
2024-01-19 16:03:49 -0500

    fidgata; scaling: means->data, store as DataArrays

--- bgk/autofigs/figdata.py
+++ bgk/autofigs/figdata.py
@@ -1,6 +1,8 @@
 import yaml
 from functools import cache
 
+import xarray as xr
+
 from bgk.typing import BpVariableName
 
 __all__ = ["OriginMeans"]
@@ -16,14 +18,14 @@ def _get_yml(yml_path: str) -> dict[str, dict]:
 
 
 class OriginMeans:
-    axis_t: list[float]
-    means: list[float]
+    axis_t: xr.DataArray
+    data: xr.DataArray
 
     def __init__(self, case: str, variable_name: BpVariableName, yml_path: str = "figdata.yml") -> None:
         """`case`: e.g. `"B00.10-n1024"`.\n\n `param`: e.g. `"ne"`"""
         self.case = case
         self.variable_name = variable_name
 
-        data = _get_yml(yml_path)[case]
-        self.axis_t = data["times"]
-        self.means = data["origin_means"][variable_name]
+        item = _get_yml(yml_path)[case]
+        self.data = xr.DataArray(data=item["origin_means"][variable_name], coords=[("t", item["times"])])
+        self.axis_t = self.data.t
