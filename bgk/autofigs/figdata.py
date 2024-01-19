a89af67a6a7c5dbf26c9e174ac7602849542f8d2
James <jm1667@wildcats.unh.edu>
2024-01-19 15:56:19 -0500

    figdata: param->variable_name

--- bgk/autofigs/figdata.py
+++ bgk/autofigs/figdata.py
@@ -1,6 +1,8 @@
 import yaml
 from functools import cache
 
+from bgk.typing import BpVariableName
+
 __all__ = ["OriginMeans"]
 
 
@@ -17,11 +19,11 @@ class OriginMeans:
     times: list[float]
     means: list[float]
 
-    def __init__(self, case: str, param: str, yml_path: str = "figdata.yml") -> None:
+    def __init__(self, case: str, variable_name: BpVariableName, yml_path: str = "figdata.yml") -> None:
         """`case`: e.g. `"B00.10-n1024"`.\n\n `param`: e.g. `"ne"`"""
         self.case = case
-        self.param = param
+        self.variable_name = variable_name
 
         data = _get_yml(yml_path)[case]
         self.times = data["times"]
-        self.means = data["origin_means"][param]
+        self.means = data["origin_means"][variable_name]
