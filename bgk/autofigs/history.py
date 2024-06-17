65069652c431f9ed92842a673ed3c71cf640333f
James <jm1667@wildcats.unh.edu>
2024-06-17 11:29:34 -0400

    history: hopefully fix to use AIIs directly

--- bgk/autofigs/history.py
+++ bgk/autofigs/history.py
@@ -6,7 +6,7 @@ from .config import AutofigsInstructionItem, AutofigsConfig
 from .options import SETTINGS, FIGURE_TYPES, METASETTINGS
 
 
-def _find_item_setting_differences(item1: dict, item2: dict) -> list[str]:
+def _find_item_setting_differences(item1: AutofigsInstructionItem, item2: AutofigsInstructionItem) -> list[str]:
     diffs = []
     for setting in SETTINGS:
         if item1[setting] != item2[setting]:
@@ -14,7 +14,7 @@ def _find_item_setting_differences(item1: dict, item2: dict) -> list[str]:
     return diffs
 
 
-def _get_settings_as_str(item: dict, settings: list[str] = SETTINGS) -> str:
+def _get_settings_as_str(item: AutofigsInstructionItem, settings: list[str] = SETTINGS) -> str:
     return " ".join(f"{setting}={item[setting]}" for setting in settings)
 
 
@@ -29,7 +29,7 @@ def _figlist_duplicates_removed(fig_list: list[str] | list[list[str]]) -> list:
     return [list(fig) for fig in fig_set]
 
 
-def _update_figure_lists(old_item: dict, new_item: dict):
+def _update_figure_lists(old_item: AutofigsInstructionItem, new_item: AutofigsInstructionItem):
     for fig_type in FIGURE_TYPES:
         if new_item.get(fig_type, []):
             old_item[fig_type] = _figlist_duplicates_removed(old_item.get(fig_type, []) + new_item[fig_type])
@@ -47,23 +47,23 @@ class History:
         old_items_same_path = [old_item for old_item in self.history.instructions if old_item.path == path]
 
         for old_item in old_items_same_path:
-            if not _find_item_setting_differences(old_item, new_item._instruction_item):
-                _update_figure_lists(old_item, new_item._instruction_item)
+            if not _find_item_setting_differences(old_item, new_item):
+                _update_figure_lists(old_item, new_item)
                 break
         else:
             print(f"New settings used for {path} ({len(old_items_same_path)} old settings found)")
             for old_item in old_items_same_path:
                 print(f"  Old: " + _get_settings_as_str(old_item))
-            print(f"  New: " + _get_settings_as_str(new_item._instruction_item))
+            print(f"  New: " + _get_settings_as_str(new_item))
 
-            if warn and new_item._instruction_item["output_directory"] in {old_item["output_directory"] for old_item in old_items_same_path}:
+            if warn and new_item.output_directory in {old_item.output_directory for old_item in old_items_same_path}:
                 answer = input("Generating this figure will override an existing figure made using different settings. Continue? Y/n ")
                 while answer.lower() not in {"", "y", "yes", "n", "no"}:
                     answer = input('Invalid response. Answer "yes" (default) or "no": ')
                 if answer.lower() in {"n", "no"}:
                     exit(0)
 
-            self.history.instructions.append(new_item._instruction_item)
+            self.history.instructions.append(new_item)
 
     def save(self):
         self.history.save(self.path)
